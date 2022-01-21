ec67df78a75f15986380d6caa49acc9cacaf4c25
Caio Santos <caio.santos@msn.com>
2022-01-21 21:09:42 +0900

    add cities json

---
+++ data/cities.js
@@ -0,0 +1,9 @@
+const cities = [];
+
+const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+fetch(url).then(city => city.json())
+  .then(data => cities.push(...data));
+
+
+export default cities;
--- data/flats.js
+++ data/flats.js
@@ -49,4 +49,7 @@ const flats = [
   }
 ];
 
+// const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+// export default endpoint;
 export default flats;
